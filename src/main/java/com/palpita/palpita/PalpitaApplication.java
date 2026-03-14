8d8cd4597b4cdfed3e9e2299ea0f278d6a5ebe3e
Lucas Lima <lucaslima@MacBook-Pro-de-Lucas.local>
2026-03-14 01:14:21 -0300

    initial commit

---
+++ src/main/java/com/palpita/palpita/PalpitaApplication.java
@@ -0,0 +1,12 @@
+package com.palpita.palpita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalpitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalpitaApplication.class, args);
+	}
+}
