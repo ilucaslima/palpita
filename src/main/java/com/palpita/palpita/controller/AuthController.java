d1f2f7a79bb3e12b5d50f20a3d6f98460d9b86c5
Lucas Lima <lucaslima@MacBook-Pro-de-Lucas.local>
2026-03-14 11:12:04 -0300

    feat: userdetailsservice implementation

--- src/main/java/com/palpita/palpita/controller/AuthController.java
+++ src/main/java/com/palpita/palpita/controller/AuthController.java
@@ -1,48 +1,39 @@
 package com.palpita.palpita.controller;
 
-import com.palpita.palpita.entity.User;
 import com.palpita.palpita.security.JwtService;
-import com.palpita.palpita.service.UserService;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.security.authentication.*;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-  private final UserService userService;
+
+  private final AuthenticationManager authenticationManager;
   private final JwtService jwtService;
-  private final PasswordEncoder passwordEncoder;
 
   public AuthController(
-      UserService userService,
-      JwtService jwtService,
-      PasswordEncoder passwordEncoder
+      AuthenticationManager authenticationManager,
+      JwtService jwtService
   ){
-    this.userService = userService;
+    this.authenticationManager = authenticationManager;
     this.jwtService = jwtService;
-    this.passwordEncoder = passwordEncoder;
   }
 
   @PostMapping("/login")
-  public Map<String, String> login(@RequestBody Map<String, String> body){
+  public Map<String, String> login(@RequestBody Map<String,String> body){
+
     String email = body.get("email");
     String password = body.get("password");
 
+    authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(email,password)
+    );
 
-    Optional<User> user = userService.findByEmail(email);
+    String token = jwtService.generateToken(email);
 
-    if(user.isPresent() && passwordEncoder.matches(password, user.get().getPassword())){
-      String token = jwtService.generateToken(email);
-
-      return Map.of("token", token);
-    }
-
-    throw new RuntimeException("Invalid credentials");
+    return Map.of("token", token);
   }
 }
